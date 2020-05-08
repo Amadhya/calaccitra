60faac1997de4c258b2f7391530cf5710071ed88
Amadhya <eshuanand07@gmail.com>
2020-05-08 09:10:06 +0530

    Review table added

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,2 +1,3 @@
 from .users import *
 from .movies import *
+from .reviews import *
---
+++ api/models/reviews.py
@@ -0,0 +1,52 @@
+import uuid
+
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+from .users import User
+from .movies import Movie
+
+
+class ReviewManager(models.Manager):
+    def get_by_user_id(self, user):
+        return self.filter(user=user)
+
+    def filter_by_movie(self, movie):
+        return self.filter(movie=movie)
+
+    def filter_by_rating(self, rating):
+        return self.filter(rating=rating)
+
+    def get_by_id(self, review_id):
+        return self.filter(id=review_id).first()
+
+
+class Review(models.Model):
+    id = models.UUIDField(primary_key=True, null=False, default=uuid.uuid4, unique=True)
+    comment_text = models.TextField(null=False)
+    rating = models.IntegerField(null=False,default=0,validators=[MaxValueValidator(5), MinValueValidator(0)])
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True, null=False)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, db_index=True, null=False)
+
+    objects = ReviewManager()
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'comment_text': self.comment_text,
+            'user': self.user.serialize(),
+            'movie': self.movie.serialize(),
+            'rating': self.rating,
+        }
+
+    @classmethod
+    def create(cls, **kwargs):
+        review = Review(
+            comment_text=kwargs.get('comment_text'),
+            user=kwargs.get('user'),
+            movie=kwargs.get('movie'),
+            rating=kwargs.get('rating'),
+        )
+
+        review.save()
+        return review
